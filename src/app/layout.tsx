afcc17a62042360ed46b0db8030d5720d622cd9a
Salat-Tamas <tamas.salat2004@gmail.com>
2025-02-28 21:52:22 +0200

    Refactor layout component by removing Sidebar and simplifying structure; adjust Toaster position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes';
 import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
 import '@uiw/react-md-editor/markdown-editor.css';
@@ -32,34 +31,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange suppressHydrationWarning>
             <div className="min-h-screen">
               <Navbar />
               <main className="py-8">
                 <div className="max-w-7xl mx-auto px-4">
-                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">
-                      <Sidebar /> 
-                    </div>
-                    <div className="lg:col-span-9">
-                      {children}
-                    </div>
-                  </div>
+                  {children}
                 </div>
               </main>
             </div>
-            <Toaster />
+            <Toaster position="bottom-right" />
           </ThemeProvider>
         </body>
       </html>
-      </ClerkProvider>
+    </ClerkProvider>
   );
 }
