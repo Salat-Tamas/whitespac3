0f01c0e9501fc8c001f8cbbc12ff99777d73b181
Anita-Mokus <csizmadia.laszlo@student.ms.sapientia.ro>
2025-02-28 23:53:14 +0200

    User sync logic in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,40 +2,62 @@ import Link from 'next/link'
 import DesktopNavbar from './DesktopNavbar'
 import MobileNavbar from './MobileNavbar'
 import { currentUser } from '@clerk/nextjs/server';
-import { getCsrfToken } from '@/utils/csrf';
 
 async function Navbar() {
     const user = await currentUser();
-    const { id, imageUrl, username, emailAddresses } = user || {};
+    const { id, imageUrl, username, emailAddresses, firstName, lastName } = user || {};
     const email = emailAddresses ? emailAddresses[0].emailAddress : '';
+    const defaultName = email ? email.split('@')[0] : '';
 
     if (user) {
       const sendUserData = async () => {
         try {
-          const csrfToken = await getCsrfToken();
-          // TO-DO: Replace api url
-          const response = await fetch(`${process.env.FASTAPI_URL}/create_user`, {
+          console.log('Syncing user data...');
+          
+          const apiUrl = process.env.FASTAPI_URL;
+          const secretKey = process.env.SECRET_KEY || '';
+
+          if (!apiUrl) {
+            throw new Error('FASTAPI_URL is not configured');
+          }
+
+            const response = await fetch(`${apiUrl}/create_user`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-              'X-CSRF-Token': csrfToken,
+              'csrf-token': secretKey,
             },
             body: JSON.stringify({
-              clerk_id: id,
-              image_url: imageUrl,
-              username: username,
-              email: email
+              id: id,
+              imageUrl: imageUrl,
+              userName: username ?? 'userName',
+              email: email,
+              name: ((firstName ?? '') + (lastName ?? '')).trim() || defaultName
             }),
-          });
+            });
+
+          if(response.status === 400){
+            console.error("User already exists");
+          }
+          // console.log(response.status);
+          // console.log({
+          //   id: id,
+          //   imageUrl: imageUrl,
+          //   userName: username,
+          //   email: email,
+          //   name: ((firstName ?? '') + (lastName ?? '')).trim() || defaultName
+          // });
 
           if (!response.ok) {
-            throw new Error('Failed to sync user data');
+            console.log('response: ' + JSON.stringify(response));
+            // throw new Error('Failed to sync user data');
           }
+
         } catch (error) {
           console.error('Error syncing user data:', error);
         }
       };
-
+   
       if (id) {
         sendUserData();
       }
